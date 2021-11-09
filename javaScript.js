0ef8f430c4546516730d1c81e76cb2d63f9bdd17
Alex Oliveira <alexskntks@outlook.com>
2021-11-09 16:15:01 -0300

    criando homePage
    Alteração do menu

---
+++ javaScript.js
@@ -0,0 +1,19 @@
+function changeBG(op) {
+	if(op === 1){
+		document.getElementById('socio1').style.backgroundColor = "white";
+
+	}else{
+		document.getElementById('socio2').style.backgroundColor = "white";
+	}
+
+}	
+
+function oldColor(op) {
+	if(op === 1){
+		document.getElementById('socio1').style.backgroundColor = "#9E8FCA";
+
+	}else{
+		document.getElementById('socio2').style.backgroundColor = "#9E8FCA";
+	}
+	
+}
