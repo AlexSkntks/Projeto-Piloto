43022923eff1d2175c5c033ff336a91368ae3ca0
Alex Oliveira <alexskntks@outlook.com>
2021-11-25 17:32:00 -0300

    testes
    Sistema

--- server.js
+++ server.js
@@ -4,18 +4,35 @@ let ejs = require('ejs');
 let app = express()
 let port = 3000
 const path  = require("path")
+const bodyParser = require('body-parser')
 
 app.use(express.static(path.join(__dirname,"public")))
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname);
 
-let login = false;
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//Variável que controla a exibicão do menu de um usuário logado
+let login = true;
 
 /*Rotas */
 app.get('/', (req, res) => {
   	//res.sendFile(__dirname + "/index.html");
 	res.render(__dirname + "/index.html", {isLogged: login});/*Usando EJS para passar algum valor para o lado do cliente*/
+})
+
+app.post('/', (req, res) => {
+
+	if(req.body.login === "admin" && req.body.senha === "123"){
+		login = true;
+		res.render(__dirname + "/index.html", {isLogged: login});/*Usando EJS para passar algum valor para o lado do cliente*/
+	}else{
+		res.render(__dirname + "/login.html", {isLogged: login});/*Usando EJS para passar algum valor para o lado do cliente*/
+	}
+	console.log(req.body.login)
 })
 
 app.get('/socio1', (req, res) => {
